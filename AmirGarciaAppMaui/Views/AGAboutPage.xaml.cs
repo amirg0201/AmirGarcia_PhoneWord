fcaf377b1cf90b2f08fc6310236faf3753a18a7c
(Estudiante) Amir Alejandro García Villamarín <amir.garcia@udla.edu.ec>
2024-06-16 17:24:45 -0500

    Actualizacion a patron MVVM

--- AmirGarciaAppMaui/Views/AGAboutPage.xaml.cs
+++ AmirGarciaAppMaui/Views/AGAboutPage.xaml.cs
@@ -7,12 +7,4 @@ public partial class AGAboutPage : ContentPage
 		InitializeComponent();
 	}
 
-    private async void AgLearnMore_Clicked(object sender, EventArgs e)
-    {
-        if (BindingContext is Models.AgAbout about)
-        {
-            // Navigate to the specified URL in the system browser.
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
-    }
 }
